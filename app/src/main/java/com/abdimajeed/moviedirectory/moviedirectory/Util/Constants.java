379228ce2b67685204b0a48341d129ae18ec0339
Majeed Akinlusi <ada6@illinois.edu>
2020-04-24 15:15:08 -0500

    first commit

---
+++ app/src/main/java/com/abdimajeed/moviedirectory/moviedirectory/Util/Constants.java
@@ -0,0 +1,14 @@
+package com.abdimajeed.moviedirectory.moviedirectory.Util;
+
+
+/**
+ * Created by Majeed Akinlusi on 4/23/20.
+ */
+
+public class Constants {
+
+    public static final String URL_LEFT = "http://www.omdbapi.com/?s=";
+    public static final String URL = "http://www.omdbapi.com/?i=";
+    public static final String API_KEY = "&apikey=91cc91ea";
+    public static final String URL_RIGHT = "&page=2";
+}
