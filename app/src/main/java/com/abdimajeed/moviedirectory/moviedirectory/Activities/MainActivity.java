e6f668233852ce931a72852c94dd1e022d1f460e
Majeed Akinlusi <ada6@illinois.edu>
2020-04-24 15:15:29 -0500

    first commit

--- app/src/main/java/com/abdimajeed/moviedirectory/moviedirectory/Activities/MainActivity.java
+++ app/src/main/java/com/abdimajeed/moviedirectory/moviedirectory/Activities/MainActivity.java
@@ -1,5 +1,5 @@
 package com.abdimajeed.moviedirectory.moviedirectory.Activities;
-
+//
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
